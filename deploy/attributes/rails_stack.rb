afcb6c1a6eff157994582eae4f1cb27d0eb17439
Alex Scarborough <alex@gameclay.com>
2013-05-01 12:13:25 -0700

    Revert "Try sudo clean-restarting Unicorn"
    This reverts commit f4ad60f87942d2ca743c505b1f5727ac95917d68.

--- deploy/attributes/rails_stack.rb
+++ deploy/attributes/rails_stack.rb
@@ -9,7 +9,7 @@ when "nginx_unicorn"
   default[:opsworks][:rails_stack][:recipe] = "unicorn::rails"
   default[:opsworks][:rails_stack][:needs_reload] = true
   default[:opsworks][:rails_stack][:service] = 'unicorn'
-  default[:opsworks][:rails_stack][:restart_command] = 'sudo ../../shared/scripts/unicorn clean-restart'
+  default[:opsworks][:rails_stack][:restart_command] = '../../shared/scripts/unicorn clean-restart'
 else
   raise "Unknown stack: #{node[:opsworks][:rails_stack][:name].inspect}"
 end
