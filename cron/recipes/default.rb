bb57cd5d46c8c4fe0afd35ba320a47a55188c8b5
Sean Lerner <seanslerner@gmail.com>
2013-08-14 16:16:16 -0700

    fix cron take 2

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,13 +1,11 @@
 node[:cron].each do |task|
 
-  cron %Q["#{task[:description]}"] do
-    minute %Q[#{task[:minute]}"] if task[:minute]
-    hour %Q[#{task[:hour]}"] if task[:hour]
-    weekday %Q["#{task[:weekday]}"] if task[:weekday]
+  cron "#{task[:description]}" do
+    minute "'#{task[:minute]}'" if task[:minute]
+    hour "'#{task[:hour]}'" if task[:hour]
+    weekday "'#{task[:weekday]}'" if task[:weekday]
+    command "#{task[:command]}"
 
-    command %Q["#{task[:command]}"]
-
-    action :create
   end
 
 end
