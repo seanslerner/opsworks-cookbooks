8b15788d19b8d91e01c0d365e15fda6b6ec6b30b
Sean Lerner <seanslerner@gmail.com>
2013-08-14 20:53:38 -0700

    execute delayed job start

--- delayed_job/recipes/default.rb
+++ delayed_job/recipes/default.rb
@@ -1,5 +1,6 @@
 execute "start_delayed_job_workers" do
   command %Q{
-    ruby ../srv/www/switchboard_unicorn/current/script/job_runner
+    cd ../srv/www/switchboard_unicorn/current &&
+    RAILS_ENV=production script/delayed_job start
   }
 end
