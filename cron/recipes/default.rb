45d7aec29a80318b8ad063c24b998442bd9b71e5
Sean Lerner <seanslerner@gmail.com>
2013-08-14 17:55:24 -0700

    update cron tasks

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,4 +1,15 @@
-cron "test_cron" do
-  minute "*/1"
-  command "echo TEST"
+cron "send_email_subscriptions" do
+  hour "24"
+  weekday "0"
+  command %Q{cd ../srv/www/switchboard_unicorn/current/ &&
+    bundle exec rake email_subscription_scheduler
+  }
+end
+
+cron "geocode_locations" do
+  hour "23"
+  weekday "0"
+  command %Q{cd ../srv/www/switchboard_unicorn/current/ &&
+    bundle exec rake geocode_update
+  }
 end
