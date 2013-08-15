ec2a190a5965e8511ff1ea26e1d4d7b66155e971
Sean Lerner <seanslerner@gmail.com>
2013-08-14 17:04:10 -0700

    test cron

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,11 +1,4 @@
-node[:cron].each do |task|
-
-  cron "#{task[:description]}" do
-    minute "'#{task[:minute]}'" if task[:minute]
-    hour "'#{task[:hour]}'" if task[:hour]
-    weekday "'#{task[:weekday]}'" if task[:weekday]
-    command "#{task[:command]}"
-
-  end
-
+cron "test_cron" do
+  minute "*/1"
+  command "echo TEST"
 end
