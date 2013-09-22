ae20098af869fcf76dcb00f09161fd4c05451e94
Sean S Lerner <seanslerner@gmail.com>
2013-09-22 09:54:46 -0700

    fix cron

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -2,16 +2,11 @@ cron "send_email_subscriptions" do
   minute "0"
   hour "5"
   day "*"
-  command 'cd ../srv/www/switchboard_unicorn/current/ && bundle exec rake email_subscription_scheduler'
+  command 'cd ../../srv/www/switchboard_unicorn/current/ && bundle exec rake email_subscription_scheduler'
 end
 
 cron "geocode_locations" do
   hour "22"
   weekday "0"
-  command 'cd ../srv/www/switchboard_unicorn/current/ && bundle exec rake geocode_update'
+  command 'cd ../../srv/www/switchboard_unicorn/current/ && bundle exec rake geocode_update'
 end
-
-# cron "remove_stale_communities" do
-#   hour "3"
-#   command 'cd ../srv/www/switchboard_unicorn/current/ && bundle exec rake cleanup:communities'
-# end
