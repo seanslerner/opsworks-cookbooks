711811fb82d0872e55539d42ef815095bb7840c3
Sean S Lerner <seanslerner@gmail.com>
2013-10-02 23:02:06 -0700

    add sportngin/elasticsearch

--- elasticsearch/recipes/install.rb
+++ elasticsearch/recipes/install.rb
@@ -1,16 +1,17 @@
-elasticsearch = "elasticsearch-#{node.elasticsearch[:version]}"
+#-*- encoding : utf-8 -*-
+elasticsearch = "elasticsearch-#{node[:elasticsearch][:version]}"
 include_recipe "elasticsearch::packages"
 
 # Create user and group
-group node.elasticsearch[:user] do
+group node[:elasticsearch][:user] do
   action :create
 end
 
-user node.elasticsearch[:user] do
+user node[:elasticsearch][:user] do
   comment "ElasticSearch User"
   home    "/home/elasticsearch"
   shell   "/bin/bash"
-  gid     node.elasticsearch[:user]
+  gid     node[:elasticsearch][:user]
   supports :manage_home => false
   action  :create
 end
@@ -21,23 +22,13 @@ script "install_elasticsearch" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-    wget "#{node.elasticsearch[:download_url]}"
-    tar xvzf #{node.elasticsearch[:filename]} -C #{node.elasticsearch[:dir]}
-    ln -s "#{node.elasticsearch[:home_dir]}-#{node.elasticsearch[:version]}" #{node.elasticsearch[:home_dir]}
+    wget "#{node[:elasticsearch][:download_url]}"
+    tar xvzf #{node[:elasticsearch][:filename]} -C #{node[:elasticsearch][:dir]}
+    ln -s "#{node[:elasticsearch][:home_dir]}-#{node[:elasticsearch][:version]}" #{node[:elasticsearch][:home_dir]}
   EOH
 end
 
 # Create Symlink
-  link "#{node.elasticsearch[:home_dir]}" do
-  to "#{node.elasticsearch[:home_dir]}-#{node.elasticsearch[:version]}"
-end
-
-# Install Kuromoji plugin
-script "install_plugins" do
-  interpreter "bash"
-  user "root"
-  cwd "#{node.elasticsearch[:home_dir]}"
-  code <<-EOH
-    bin/plugin -install elasticsearch/elasticsearch-analysis-kuromoji/1.1.0
-  EOH
+  link "#{node[:elasticsearch][:home_dir]}" do
+  to "#{node[:elasticsearch][:home_dir]}-#{node[:elasticsearch][:version]}"
 end
