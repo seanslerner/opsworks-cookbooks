b452488facc8fd44a4dc20060753448b07e9bab7
Sean S Lerner <seanslerner@gmail.com>
2013-10-30 13:43:25 -0700

    update cron

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,12 +1,11 @@
 cron "send_email_subscriptions" do
-  minute "0"
-  hour "5"
+  hour "15"
   day "*"
-  command 'cd ../../srv/www/switchboard_unicorn/current/ && bundle exec rake email_subscription_scheduler'
+  command "cd /srv/www/switchboard_unicorn/current/ && bundle exec rake email_subscription_scheduler"
 end
 
 cron "geocode_locations" do
-  hour "22"
+  hour "8"
   weekday "0"
-  command 'cd ../../srv/www/switchboard_unicorn/current/ && bundle exec rake geocode_update'
+  command "cd /srv/www/switchboard_unicorn/current/ && bundle exec rake geocode_update"
 end
