4b93bf543cf96458bf8a74637226e1163d85a6ec
Sean S Lerner <seanslerner@gmail.com>
2013-12-02 10:57:34 -0800

    hack to ensure shared assets exists

--- opsworks_custom_env/recipes/write_config.rb
+++ opsworks_custom_env/recipes/write_config.rb
@@ -2,11 +2,21 @@
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
 node[:deploy].each do |application, deploy|
-  
+
   custom_env_template do
     application application
     deploy deploy
     env node[:custom_env][application]
   end
-  
+
+  Chef::Log.info("Ensuring shared/assets directory for #{application} app...")
+
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode 0775
+    action :create
+    recursive true
+  end
+
 end
