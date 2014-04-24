2de2dcf8a31ff3e0e988c1f7c51cc66689ecc94d
Sean S Lerner <seanslerner@gmail.com>
2014-04-24 11:38:35 -0700

    update cron

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -2,5 +2,5 @@ cron "send_email_alerts" do
   minute  "10"
   hour    "15"
   day     "*"
-  command "cd /srv/www/switchboard/current && bundle exec rake email_subscription_scheduler"
+  command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production email_subscription_scheduler"
 end
