676f8c1f0054d83918ec7f097d04cde6b30bb623
Sean S Lerner <seanslerner@gmail.com>
2014-06-05 08:47:22 -0700

    update cron jobs

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -4,3 +4,10 @@ cron "send_email_alerts" do
   day     "*"
   command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production email_subscription_scheduler"
 end
+
+cron "calculate_scores" do
+  minute  "0"
+  hour    "4"
+  day     "*"
+  command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production calculate_scores"
+end
