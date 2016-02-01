265b230728b51ea205ce151a2221cc1b42016a6a
Sean S Lerner <seanslerner@gmail.com>
2016-02-01 11:41:26 -0800

    updates crons

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -33,12 +33,12 @@ cron "promote_posts" do
   command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production promotions"
 end
 
-cron "parse_logs" do
-  minute  "20"
-  hour    "*"
-  day     "*"
-  command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production parse_event_log"
-end
+# cron "parse_logs" do
+#   minute  "20"
+#   hour    "*"
+#   day     "*"
+#   command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production parse_event_log"
+# end
 
 cron "process_email_queue" do
   minute "30"
@@ -61,12 +61,12 @@ cron "check_card_expiration" do
   command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production gold:check_card_expiration"
 end
 
-cron "summarize_all_communities" do
-  minute  "0"
-  hour    "15"
-  day     "*"
-  command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production argus:summarize_all_communities"
-end
+# cron "summarize_all_communities" do
+#   minute  "0"
+#   hour    "15"
+#   day     "*"
+#   command "cd /srv/www/switchboard/current && /usr/local/bin/rake RAILS_ENV=production argus:summarize_all_communities"
+# end
 
 cron "summarize all communities' user activity (OrcKillah)" do
   minute  "0"
